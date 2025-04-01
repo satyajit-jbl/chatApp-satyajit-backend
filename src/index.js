2cd95e9570b64db65f965bbc103358a7f5902e3f
satyajit <satyajit_jbl@yahoo.com>
2025-04-02 03:18:10 +0600

    first commit after auth api created

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// const express = require("express") (by adding  "type": "module" we can import instead)
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js" //এখানে authRoutes হলো একটি নামমাত্র অ্যাসাইনমেন্ট, যেটি router কে ধরে রেখেছে। অর্থাৎ, যেহেতু auth.route.js এ default export করা হয়েছে, তাই import করার সময় যেকোনো নাম ব্যবহার করা যায়।
+// import { connect } from "mongoose";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+//to test the server
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(PORT, ()=>{
+    console.log("Server is running on PORT:" +PORT);
+    connectDB()
+});
