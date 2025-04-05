bf21507041bd5f011209e20769caf194eb2b74de
satyajit <satyajit_jbl@yahoo.com>
2025-04-05 21:28:13 +0600

    socket io setup done, connection and disconection testing with ui done

--- src/index.js
+++ src/index.js
@@ -8,28 +8,29 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js" //এখানে authRoutes হলো একটি নামমাত্র অ্যাসাইনমেন্ট, যেটি router কে ধরে রেখেছে। অর্থাৎ, যেহেতু auth.route.js এ default export করা হয়েছে, তাই import করার সময় যেকোনো নাম ব্যবহার করা যায়।
 // import { connect } from "mongoose";
 import messageRoutes from "./routes/message.route.js" 
+import { app, server } from "./lib/socket.js";
 
 
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5175",
+    origin: "http://localhost:5173",
     credentials: true,
 }))
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 //to test the server
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log("Server is running on PORT:" +PORT);
     connectDB()
 });
